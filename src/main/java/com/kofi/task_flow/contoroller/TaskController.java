60bd079521437e9b0099241eda39af62d541a3e6
Kofidhope <forsonbentum55@gmail.com>
2026-09-13 00:48:02 +0000

    feat: add get task by id endpoint

--- src/main/java/com/kofi/task_flow/contoroller/TaskController.java
+++ src/main/java/com/kofi/task_flow/contoroller/TaskController.java
@@ -3,6 +3,7 @@ package com.kofi.task_flow.contoroller;
 import com.kofi.task_flow.model.Task;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class TaskController {
 
     @GetMapping
     public ResponseEntity<Task> getTask(String id) {
+        return ResponseEntity.ok().body(new Task());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         return ResponseEntity.ok().body(new Task());
     }
 
