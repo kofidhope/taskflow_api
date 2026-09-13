ebe07a27e0c9ec2010a30144c40f1ccfce270f60
Kofidhope <forsonbentum55@gmail.com>
2026-09-13 00:33:14 +0000

    chore: initialize Spring Boot TaskFlow API project

---
+++ src/test/java/com/kofi/task_flow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kofi.task_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
