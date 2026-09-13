c64e012168c528ce8dcc902e91b08de12e259f8c
Kofidhope <forsonbentum55@gmail.com>
2026-09-13 01:41:49 +0000

    refactor: move task logic into service

--- src/main/java/com/kofi/task_flow/service/TaskService.java
+++ src/main/java/com/kofi/task_flow/service/TaskService.java
@@ -1,7 +1,14 @@
 package com.kofi.task_flow.service;
 
+import com.kofi.task_flow.model.Task;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TaskService {
+
+    private final Map<Long, Task> tasks = new HashMap<>();
+
 }
